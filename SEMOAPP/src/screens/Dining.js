07d604757e181c9f45e4c155652f069244b59255
SilentEarth32 <54873458+SilentEarth32@users.noreply.github.com>
2022-03-28 18:42:52 -0500

    title for start page of dining

--- SEMOAPP/src/screens/Dining.js
+++ SEMOAPP/src/screens/Dining.js
@@ -1,19 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, Image, View } from 'react-native';
-import { styles } from '../styles/LandingStyle';
+import { styles } from '../styles/DiningStyle';
+import { Tile, HeaderTile, NewsTile } from '../components/Tile';
+
 export default function Dining() {
   return (
     <View style={styles.container}>
       <View style={styles.headerContainer}>
         <View style={styles.headerSubContainer}>
-          <Text>Heading</Text>
+          <Text style={[styles.pickTitle, {fontFamily: 'Times', color:'white'}]}>Choose a Location...</Text>
          </View>
       </View>
       
       <View style={styles.tileContainer}>
-        <Text>Dining</Text>
+        <View style={styles.diningTileSub}>
+          <Text>Dining</Text>
+          <Tile name={"Lab Availability"}  fullscreen={false} />
+          <Tile name={"Athletics"}  fullscreen={false} />
+          <Tile name={"Student Government"}  fullscreen={true} />
         </View>
-        </View>
+      </View>
+    </View>
     
   );
 }
