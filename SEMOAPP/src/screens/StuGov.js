7d0234624c81107796f3ac9913d240a794d6603f
Zllocke1s <64339501+Zllocke1s@users.noreply.github.com>
2022-04-19 10:19:50 -0500

    added minuteItem

--- SEMOAPP/src/screens/StuGov.js
+++ SEMOAPP/src/screens/StuGov.js
@@ -4,15 +4,45 @@ import { styles } from '../styles/StuGovStyle';
 import { MinuteItem } from '../components/MinuteItem';
 import React, { useEffect } from 'react';
 import { theme } from '../core/theme';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import Moment from 'moment';
+
 export default function StuGov() {
 
   const [minutes, setMinutes] = React.useState(null)
   const [agenda, setAgenda] = React.useState(null)
   const [formattedItems, setFormattedItems] = React.useState(null);
   const [isLoaded, setIsLoaded] = React.useState(false);
+  const [aID, setAID] = React.useState(0)
+  const [pDates, setPDates] = React.useState([])
+  const colors = [theme.colors.red, theme.colors.blue]
 
+  const fetchAgenda = () => {
+    setIsLoaded(false);
+    fetch("https://selink.semo.edu/legacy/webapi/drive/folder/1969729/contents?page=1&pageSize=30", {
+      "headers": {
+        "accept": "application/json, text/plain, */*",
+        "accept-language": "en-US,en;q=0.9",
+        "groupkey": "studentgov",
+        "sec-fetch-dest": "empty",
+        "sec-fetch-mode": "cors",
+        "sec-fetch-site": "same-origin",
+        "x-xsrf-token": "iLMWswYnBAZy_PjiWfmgFJqBHmySqJ-Sq0oMvgz_vllEPkP0jYov3YOxlOTs8AOe6gNXqSVEAuQ_AfwbO2XcO6cjIM8MdsLOHuoJ6SHbbOM1:iFBuM3am8qgVx7HxrWYqJ2NgLrXzDnA9TA-FmNtlvRcUbtIkYRZ4lmNULJii421dn8iS4rt1cQ4rMfuqAoT45RzVBbhQTTIC-zjaG-cTUkM1"
+      },
+      "referrer": "https://selink.semo.edu/organization/studentgov/documents",
+      "referrerPolicy": "strict-origin-when-cross-origin",
+      "body": null,
+      "method": "GET",
+      "mode": "cors",
+      "credentials": "include"
+    }).then((response) => response.json())
+    .then((json) => {
+      setAgenda((json))
+    })
+  }
 
-  React.useEffect(() => {
+  const fetchMinutes = () => {
+    setIsLoaded(false);
     fetch("https://selink.semo.edu/legacy/webapi/drive/folder/1973483/contents?page=1&pageSize=30", {
       "headers": {
         "accept": "application/json, text/plain, */*",
@@ -37,42 +67,105 @@ export default function StuGov() {
       console.log(error)
     })
     
-    fetch("https://selink.semo.edu/legacy/webapi/drive/folder/1969729/contents?page=1&pageSize=30", {
-      "headers": {
-        "accept": "application/json, text/plain, */*",
-        "accept-language": "en-US,en;q=0.9",
-        "groupkey": "studentgov",
-        "sec-fetch-dest": "empty",
-        "sec-fetch-mode": "cors",
-        "sec-fetch-site": "same-origin",
-        "x-xsrf-token": "iLMWswYnBAZy_PjiWfmgFJqBHmySqJ-Sq0oMvgz_vllEPkP0jYov3YOxlOTs8AOe6gNXqSVEAuQ_AfwbO2XcO6cjIM8MdsLOHuoJ6SHbbOM1:iFBuM3am8qgVx7HxrWYqJ2NgLrXzDnA9TA-FmNtlvRcUbtIkYRZ4lmNULJii421dn8iS4rt1cQ4rMfuqAoT45RzVBbhQTTIC-zjaG-cTUkM1"
-      },
-      "referrer": "https://selink.semo.edu/organization/studentgov/documents",
-      "referrerPolicy": "strict-origin-when-cross-origin",
-      "body": null,
-      "method": "GET",
-      "mode": "cors",
-      "credentials": "include"
-    }).then((response) => response.json())
-    .then((json) => {
-      setAgenda((json))
-    })
     
+  }
+
+  React.useEffect(() => {
+    fetchAgenda()
+    fetchMinutes()
   }, [])
   
-React.useEffect(() => {
-  if(minutes!=null && agenda!=null)
-  {
-  setFormattedItems(
-    minutes.map((minute) => {
-      return(
-        <MinuteItem minutes={minute}/>
-      )
+  React.useEffect(() => {
+    if(minutes!=null)
+    {
+      var dates = [];
+      minutes.forEach((item) => {
+        const d = (item.Title.replace(new RegExp("Senate Mins[_ ]"), "").replace(".pdf", ""));
+        var dateExp = d.split("_");
+        var year = "20" + dateExp[2];
+        var month = dateExp[0].length==1 ? "0" + dateExp[0] : dateExp[0];
+        var day = dateExp[1].length==1 ? "0" + dateExp[1] : dateExp[1];
+        var dateString = Moment(year + "-" + month + "-" + day).format('YYYY-MM-DD')
+        var t = item.Title;
+        var valid = Moment(year + "-" + month + "-" + day).isValid()
+        var x = valid ? dateString : item.Title;
+        dates.push({t, x})
+      });
+      minutes.sort((a, b) => {
+        var fA = dates.find((d) => d.t == a.Title)
+        var fB = dates.find((d) => d.t == b.Title)
+        return (fA.x) > (fB.x) ? 1 : -1
+      })
+
+    setFormattedItems(
+      minutes.map((minute) => {
+        return(
+          <MinuteItem type="minutes" minutes={minute}/>
+        )
+      })
+    )
+    setIsLoaded(true)
+    }
+  }, [minutes])
+
+  React.useEffect(() => {
+    if(agenda!=null)
+    {
+      var dates = [];
+      var valids = [];
+      agenda.forEach((item) => {
+        const d = (item.Title.replace(new RegExp("Senate Agenda[_ ]"), "").replace(".docx", ""));
+        var dateExp = d.split("_");
+        var year = "20" + dateExp[2];
+        var month = dateExp[0].length==1 ? "0" + dateExp[0] : dateExp[0];
+        var day = dateExp[1].length==1 ? "0" + dateExp[1] : dateExp[1];
+
+        var dateString = Moment(year + "-" + month + "-" + day).format('YYYY-MM-DD')
+        var t = item.Title;
+        var valid = Moment(year + "-" + month + "-" + day).isValid()
+        var x = valid ? dateString : item.Title;
+        //console.log(x)
+        dates.push({t, x})
+      });
+      setPDates(dates)
+      agenda.sort((a, b) => {
+        
+        var fA = dates.find((d) => d.t == a.Title)
+        var fB = dates.find((d) => d.t == b.Title)
+        //console.log(fA.x)
+        return (fA.x) > (fB.x) ? 1 : -1
+        
+      })
+
+    setFormattedItems(
+      agenda.map((minute) => {
+        return(
+          <MinuteItem type="agenda" minutes={minute}/>
+        )
+      })
+    )
+    setIsLoaded(true)
+    }
+  }, [agenda])
+
+  React.useEffect(() => {
+    if(aID==0)
+    {
+      fetchAgenda()
+    }
+    else
+    {
+      fetchMinutes()
+    }
+  }, [aID])
+
+
+  React.useEffect(() => {
+    fetch("https://www.instagram.com/sga_semo/").then((response) => {
+      console.log(typeof(response))
     })
-  )
-  setIsLoaded(true)
-  }
-}, [minutes, agenda])
+  }, [])
+
 
   return (
     <View style={styles.container}>
@@ -85,10 +178,13 @@ React.useEffect(() => {
       <View style={styles.galleryContainer}>
         <Image source={require("../assets/stugov.png")} style={styles.galleryImg}></Image>
         </View>
-      <View style={styles.minutesContainer}>
+        <View style={styles.tabContainer}>
+      <TouchableOpacity onPress={() => setAID(0)} style={aID==0 ? styles.redActive: styles.inactive}><Text style={styles.tabTitle}>Agendas</Text></TouchableOpacity>
+      <TouchableOpacity onPress={() => setAID(1)} style={aID==1 ? styles.blueActive : styles.inactive}><Text style={styles.tabTitle}>Minutes</Text></TouchableOpacity>
+        </View>
+      <View style={[styles.infoContainer, {borderColor: colors[aID]}]}>
         <ScrollView>
         {isLoaded ? formattedItems : <ActivityIndicator style={styles.loadingBar} size="large" color={theme.colors.red} />}
-        <MinuteItem minutes={{Title: "4_4_2022\t\t\t\t\t\t\t\tMinutes\t\t\t\t\t\t\t\t\tAgenda"}}/>
         </ScrollView>
       </View>
         </View>
--- SEMOAPP/src/styles/StuGovStyle.js
+++ SEMOAPP/src/styles/StuGovStyle.js
@@ -11,6 +11,49 @@ export const styles = StyleSheet.create({
       alignContent: "center",
       justifyContent: 'flex-start'
     },
+    tabContainer: {
+        marginBottom: -3,
+        paddingBottom: 0,
+        display: "flex",
+        flexDirection: "row",
+        width: "100%",
+        justifyContent: "space-around",
+    },
+    redActive: {
+        padding: 10,
+        paddingLeft: 60,
+        paddingRight: 60,
+        width: "100%",
+        backgroundColor: theme.colors.red,
+        
+    },
+    blueActive: {
+        padding: 10,
+        
+        paddingLeft: 60,
+        paddingRight: 60,
+        backgroundColor: theme.colors.blue,
+    },
+    inactive: {
+        padding: 10,
+        paddingLeft: 60,
+        paddingRight: 60,
+        backgroundColor: theme.colors.gray3,
+    },
+    tabTitle: {
+        fontSize: 20,
+        fontWeight: "bold",
+        color: theme.colors.gray,
+        marginTop: 0
+    },
+    infoContainer: {
+        width: "100%",
+        backgroundColor: theme.colors.gray,
+        borderTopWidth: 15,
+        borderWidth: 5,
+        flex: 0.4
+    },
+    
     headerContainer: {
         flex: .15,
         backgroundColor: theme.colors.red,
