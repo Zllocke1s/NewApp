405e2b2fa4ca839486ca92c5a20f1cb9a565eb55
Zllocke1s <64339501+Zllocke1s@users.noreply.github.com>
2022-04-19 15:49:04 -0500

    Grade code
    graed

--- SEMOAPP/src/screens/Schedule.js
+++ SEMOAPP/src/screens/Schedule.js
@@ -1,19 +1,153 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, Image, View } from 'react-native';
-import { styles } from '../styles/LandingStyle';
+import { StyleSheet, Modal, Text, Image, View, TouchableOpacity } from 'react-native';
+import { styles } from '../styles/ClassesStyle';
+import { ClassTile, GradePercentTile } from '../components/Tile';
+import { Feather } from '@expo/vector-icons'; 
+import { theme } from '../core/theme.js';
+import React, { useEffect } from 'react';
+import base64 from 'react-native-base64'
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { ActivityIndicator, Button } from 'react-native-paper';
+import { TriangleColorPicker } from 'react-native-color-picker'
+import Moment from 'moment';
+import { ScrollView } from 'react-native-gesture-handler';
+
 export default function Schedule() {
+
+
+  const [credentials, setCredentials] = React.useState(null)
+  const [classes, setClasses] = React.useState(null)
+  const [formattedclasses, setFormattedclasses] =React.useState(null)
+  const [colors, setColors] = React.useState(null)
+  const [attemptColors, setAttemptGrab] = React.useState(false)
+  const [modal, setModal] = React.useState(null)
+  const [modalVisible, setModalVisible] = React.useState(false)
+  const [modalPicker, setModalPicker] = React.useState(null)
+  useEffect(() => {
+    if(credentials!=null)
+    {
+    fetch('http://mportal.semo.edu:8080/banner-mobileserver/api/2.0/courses/calendarview/' + credentials.so,
+    {
+      headers: {
+        Authorization: 'Basic '+base64.encode(credentials.username + ":" + credentials.password), 
+    }, 
+    })
+    .then((response) => {
+      response.json().then((json) => {
+      setClasses(json.coursesDays)
+    }
+    )})
+    }
+  }, [credentials])
+
+  const getData = async (key) => {
+    try {
+      const jsonValue = await AsyncStorage.getItem(key)
+      setCredentials( jsonValue != null ? (JSON.parse(jsonValue)) : null);
+      console.log("Pulled: " + jsonValue)
+    } catch(e) {
+      console.log(e)
+      // error reading value
+    }
+  }
+
+  const getColors = async () => {
+    try {
+      const jsonValue = await AsyncStorage.getItem("gradeColors")
+      setColors( jsonValue != null ? (JSON.parse(jsonValue)) : []);
+      console.log("Colors: " + jsonValue)
+    } catch(e) {
+      console.log(e)
+      // error reading value
+    }
+  }
+  const storeColors = async () => {
+    try {
+      const jsonValue = JSON.stringify(colors)
+      await AsyncStorage.setItem("gradeColors", jsonValue)
+    } catch (e) {
+      // saving error
+    }
+    console.log("Stored")
+    
+  }
+
+  useEffect(() => {
+    if(colors==[] || colors==null)
+    {
+      console.log("No Colors set")
+      setColors([
+        "#abd67d",
+        "#a7ddda",
+        "#ff876b",
+        "#ffbfb0",
+        "#c7a1d1",
+        "#e2aedd",
+        "#ff876b"]
+      )
+    }
+    else if(colors!=[])
+    {
+      storeColors()
+    }
+  }, [colors])
+
+  useEffect(() => {
+    getData("credentials")
+    getColors()
+  }, [])
+
+
+  useEffect(() => {
+    if(classes!=null)
+    {
+      console.log("not null")
+      classes.sort((a, b) => a.date > b.date ? 1 : -1)
+    setFormattedclasses(classes.map((item, index) => {
+      return(
+        <View key={item.date} style={styles.day}>
+          <Text style={styles.doW}>{Moment(item.date).format('dddd')}</Text>
+          {item.coursesMeetings.map((cm) => {
+            console.log(cm)
+            return(
+            <View style={styles.course}>
+              <Text style={styles.className}>{cm.courseName} - {cm.courseSectionNumber}</Text>
+              <Text style={styles.classCode}>{cm.sectionTitle}</Text>
+              <View style={styles.columns}>
+              <Text>{cm.building} {cm.room}</Text>
+              <Text>{Moment(cm.start).format('h:mm')} - {Moment(cm.end).format('h:mm')}</Text>
+              </View>
+            </View>
+            )
+          })}
+        </View>
+      )
+    }))
+
+    
+  }
+  else
+  {
+    
+  }
+  }, [classes, colors])
+
+
   return (
     <View style={styles.container}>
       <View style={styles.headerContainer}>
         <View style={styles.headerSubContainer}>
-          <Text>Heading</Text>
-         </View>
+          <Text style={[styles.gradeTitle, {color:'black'}]}>Grades</Text>
+        </View>
       </View>
-      
       <View style={styles.tileContainer}>
-        <Text>hello</Text>
+        <ScrollView>
+        <View style={styles.rows}>
+            {formattedclasses != null ? formattedclasses : <ActivityIndicator size="large" color={theme.colors.red}></ActivityIndicator>}
+            </View>
+            </ScrollView>
         </View>
-        </View>
+    </View>
     
   );
 }
