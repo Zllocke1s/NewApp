07b73b111dc5a665c7e8eea4960cf95db388f6d2
Zllocke1s <64339501+Zllocke1s@users.noreply.github.com>
2022-04-27 10:45:56 -0500

    Update sizes.js

--- SEMOAPP/src/core/sizes.js
+++ SEMOAPP/src/core/sizes.js
@@ -4,7 +4,7 @@ import { Dimensions } from "react-native"
 var fullWidth = Dimensions.get('window').width;
 
 var sizes = {
-    title: fullWidth/12,
+    title: fullWidth/13,
     large: fullWidth/16
 }
 
